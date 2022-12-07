5f24fa008b246cf143c1466c0b42f4140d3e1d17
Ojakoo <ilari.ojakorpi@gmail.com>
2022-12-07 13:56:27 +0200

    Entry router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { exampleRouter } from "./example";
+import { entryRouter } from "./entry";
 
 export const appRouter = router({
   example: exampleRouter,
+  entry: entryRouter,
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/entry.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const entryRouter = router({
+  add: publicProcedure
+    .input(z.object({ 
+      amount: z.number(),
+      location: z.string(),
+      growth: z.number(),
+    }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.entry.create({data: input})
+  }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.entry.findMany();
+  }),
+});
